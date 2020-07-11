a927bef36d23494233bfbf678dd6e510089f3fa2
EsraaSalah28 <es83626@gmail.com>
2020-07-11 23:31:33 +0200

    finish blog with crud

--- models/article.js
+++ models/article.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 const articleSchema= new mongoose.Schema({
     title:{
         type: String,
@@ -11,6 +12,23 @@ const articleSchema= new mongoose.Schema({
     createdAt:{
         type: Date,
         default: Date.now
+    },
+    slug:{
+        type:String,
+         required: true,
+         unique: true   
     }
 })
+articleSchema.pre('validate', function(next) {
+    if (this.title) {
+      this.slug = slugify(this.title, { lower: true, strict: true })
+    }
+  
+    if (this.markdown) {
+      this.sanitizedHtml = dompurify.sanitize(marked(this.markdown))
+    }
+  
+    next()
+  })
+  
 module.exports=mongoose.model('Article',articleSchema)
--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express= require('express')
 const mongoose= require('mongoose')
+const Article=require('./models/article')
 const articleRouter=require('./routes/articles')
+const methodOverride= require('method-override')
 const app =express()
 mongoose.connect('mongodb://localhost/blog',{useNewUrlParser:true,useUnifiedTopology:true})
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 app.use('/articles',articleRouter)
-app.get('/',(req,res)=>
-{ const atricles =[{
-title:'Test Article',
-createdAt: new Date(),
-description:'Test description'
-
-},
-{
-  title:'Test Article2',
-  createdAt: new Date(),
-  description:'Test description 2'
-  
-  }
-]
+app.get('/',async(req,res)=>
+{ const atricles= await Article.find()
   res.render('articles/index',{atricles:atricles})
 
 }
